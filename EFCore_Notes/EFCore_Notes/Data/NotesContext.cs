d495686cafd184e8334c9e55fcc4838c3de439b1
Yuliia Tykhovska <uliatihovska1@gmail.com>
2025-08-07 10:56:48 +0300

    Added logging to file

--- EFCore_Notes/EFCore_Notes/Data/NotesContext.cs
+++ EFCore_Notes/EFCore_Notes/Data/NotesContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EFCore_Notes.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,30 @@ namespace EFCore_Notes.Data
 {
 	internal class NotesContext : DbContext
 	{
+		private readonly StreamWriter _logStream = new StreamWriter("mylog.txt", append: true);
 
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{ 
+			var configBuilder = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json")
+				.Build();
+
+			var configSection = configBuilder.GetSection("ConnectionStrings");
+
+			var connectionString = configSection["SQLServerConnection"] ?? null;
+
+			optionsBuilder.UseSqlServer(connectionString);
+
+			optionsBuilder.LogTo(_logStream.WriteLine);
+		}
+
+		public override void Dispose()
+		{
+			base.Dispose();
+			_logStream.Dispose();
+		}
+
+		public DbSet<Note> Notes { get; set; }
+		public DbSet<User> Users { get; set; }
 	}
 }
